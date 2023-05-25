906227f5165532f137e9d9c7238c8960fe0cb27c
Altyn Berdiyeva <berdiyevaaltyn@gmail.com>
2023-05-25 08:53:48 -0500

    Adding some notes on nested loops

--- script-notes/app.js
+++ script-notes/app.js
@@ -20,3 +20,20 @@
 // for (let num = 100; num >= 0; num -= 10) {
 //     console.log(num);
 // }
+
+//Nested Loops
+// for (let outer = 1; outer < 5; outer ++) {
+//     console.log(`Outer is ${outer}`);
+//     for (let inner = 3; inner > 0; inner --) {
+//         console.log(`        Inner is ${inner}`);
+//     }
+// }
+
+let seatingChart = [['Kristen', 'Erik', 'Namita'], ['Kevin', 'Joe', 'Jane', 'Olivia'], ['Yuma', 'Sakura', 'Jack', 'Steven']]
+for (let i = 0; i < seatingChart.length; i++) {
+    let row = seatingChart[i];
+    console.log(`Row ${i+1} ${row}`)
+    // for (let j = 0; j < row.length; j++) {
+    //     console.log(row[j])
+    // }
+}
