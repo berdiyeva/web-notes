4de02989c4d4d4fd00596c565f9402c9835e8957
Altyn Berdiyeva <berdiyevaaltyn@gmail.com>
2023-05-25 08:25:46 -0500

    Adding some notes on loops

--- script-notes/app.js
+++ script-notes/app.js
@@ -1,1 +1,22 @@
-console.log("Hello from the script notes!");
+// Notes on the Logical AND 
+// const password = prompt("Enter your password: ");
+// if (password.length >= 7 && password.indexOf(' ') === -1) {
+//     console.log('Valid Password!');
+// } else {
+//     console.log('The password should be at least 7 chars and no spaces!');
+// }
+
+//For loops 
+// for (let i = 1; i <= 10; i ++) {
+//     console.log(i);
+// }
+
+//Even Numbers 
+// for (let even = 0; even <=20; even += 2) {
+//     console.log(even);
+// }
+
+//Count Down 
+// for (let num = 100; num >= 0; num -= 10) {
+//     console.log(num);
+// }
