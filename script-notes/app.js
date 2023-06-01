9d72c28f1996fee4eb2d42fe973f6fc19b27fc98
Altyn Berdiyeva <berdiyevaaltyn@gmail.com>
2023-06-01 09:27:35 -0500

    Adding more function examples

--- script-notes/app.js
+++ script-notes/app.js
@@ -142,3 +142,19 @@
 // }
 // console.log(`Total: ${total}`);
 // console.log(`Average: ${total/scores.length}`)
+
+// function returnDays (num) {
+//     if (num < 1 || num > 7 ) {
+//         return null
+//     }
+//     let days = {
+//         1: "Monday",
+//         2: "Tuesday",
+//         3: "Wednesday",
+//         4: "Thursday", 
+//         5: "Friday", 
+//         6: "Saturday", 
+//         7: "Sunday"
+//     }
+//     return days[num]
+// }
